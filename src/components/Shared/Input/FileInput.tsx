a4cc600742b9c6dc94df934819c197f412b452a9
Serpentarius13 <serpentarium13@mail.ru>
2023-05-15 01:23:42 +0300

    fileinput

--- src/components/Shared/Input/FileInput.tsx
+++ src/components/Shared/Input/FileInput.tsx
@@ -1,3 +1,4 @@
+import { toaster } from "@/features/lib/toaster";
 import { ChangeEvent, useState } from "react";
 
 interface IFileInput {
@@ -20,17 +21,19 @@ export default function FileInput({
 
     if (!file) return;
 
+    if (!accept.split(",").find((el) => el.trim() === file.type))
+      return toaster.warning("Wrong file type");
     handleChange(file);
 
     setFile(file);
   }
   return (
-    <label className="flex h-[15rem] w-full max-w-[30rem] cursor-pointer items-center justify-center rounded-medium border-[1px] border-solid border-black bg-white p-[2rem] text-[2rem] font-bold transition-all hover:border-white hover:bg-gray-800 hover:text-white dark:border-white dark:bg-gray-800 dark:hover:bg-black">
+    <label className="relative flex h-[15rem] w-full max-w-[30rem] cursor-pointer items-center justify-center rounded-medium border-[1px] border-solid border-black bg-white p-[2rem] text-[2rem] font-bold transition-all hover:border-white hover:bg-gray-800 hover:text-white dark:border-white dark:bg-gray-800 dark:hover:bg-black">
       {file ? file.name : placeholder ?? "Drop your files here"}
 
       <input
         type="file"
-        className="hidden"
+        className="absolute inset-0 h-full w-full cursor-pointer opacity-0"
         onChange={handleFileInput}
         accept={accept}
       />
