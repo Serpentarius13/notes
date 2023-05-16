1e51eb7e65c33e6c74f2a5e0142eb81c07505263
Serpentarius13 <serpentarium13@mail.ru>
2023-05-16 14:23:48 +0300

    !

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,8 @@ import { NextResponse } from "next/server";
 export default async function middleware(request: Request | any) {
   const token = await getToken({ req: request, raw: true });
 
+  console.log(token, request.url);
+
   if (
     (request.url.includes("/chat") ||
       request.url.includes("/documents") ||
@@ -12,10 +14,12 @@ export default async function middleware(request: Request | any) {
       request.nextUrl.pathname === "/") &&
     !token
   ) {
+    console.log("redirect login");
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
   if (request.url.includes("/login") && token) {
+    console.log("redirect home");
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
